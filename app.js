a3174bbfdda1c94c520298e665ce17f70bb16534
hamzamekni <hamzamekni4@gmail.com>
2024-02-27 10:04:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express =require('express');
+const app = express();
+
+const authRoutes= require("./routes/auth")
+
+
+app.use(express.json());
+const mongoose = require('mongoose')
+const dotenv = require( 'dotenv');
+dotenv.config()
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 4000
+
+mongoose.connect(MONGODB_URI).then(()=> {
+    console.log('connected to the database')
+    app.listen(PORT,()=>{
+        console.log('server is running on port 4000')
+    })
+}).catch(err =>{
+    console.log('error connecting to database : ', err.message)
+})
+
+app.get('/protected',  (req, res) => {
+    res.send('Protected route accessed');
+});
+
+app.use("/auth",authRoutes)
+
+// instance variable from express
+
+
+
+
+
+
+
+
+
+
